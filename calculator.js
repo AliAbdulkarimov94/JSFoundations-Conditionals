da2243f07e3ec7c775a9d222e878144a28a2ff1b
Ali Bash MKareem <bashov94@Alis-MacBook-Pro.local>
2021-09-23 12:00:32 +0300

    I did the calculator which is awsome

--- calculator.js
+++ calculator.js
@@ -2,5 +2,26 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+const number1 = prompt("What's your first number ? ");
+const number2 = prompt("whats your seconed number  ? ");
+const MathOperation = prompt("whats your mathematical operation?");
+
+
+if (MathOperation === "+"){
+    let Results = number1 + number2
+    console.log(`${number1} + ${number2} = ${Results}`)
+}
+else if ( MathOperation === "-"){
+    let Results = number1 - number2 
+    console.log(`${number1} - ${number2} = ${Results}`)
+}
+else if (MathOperation === "*"){
+    let Results = number1 * number2
+    console.log(`${number1} * ${number2} = ${Results}`) 
+}
+else if (MathOperation === "/"){
+    let Results = number1 / number2
+    console.log(`${number1}/${number2}=${Results}`)
+}
+else 
+    console.log("Please enter a valid Mathmatical Opertation!")
